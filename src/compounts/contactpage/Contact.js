d3aec078e7e0f168eec5e6b5dec342e800ac7fef
Madhusudhan <64250324+madhusudhan123-star@users.noreply.github.com>
2023-04-11 19:52:43 +0530

    change5

--- src/compounts/contactpage/Contact.js
+++ src/compounts/contactpage/Contact.js
@@ -3,20 +3,20 @@ import "./contact.css";
 const Contact = () => {
   return(
     <div className="main_div">
-      <form form action="https://formsubmit.co/daggulamadhu63@gmail.com" method="POST" className="contact_main_div">
+      <form   action="https://formspree.io/f/xyyabjvd" method="POST" className="contact_main_div">
         <div id="contact_main_div" className="contact_div">
         <div className="name_div">
-          <input type="text" placeholder="Name" className="name" />
+          <input type="text" placeholder="Name" className="name" name='name' />
           <div className="namediv">
           </div>
         </div>
         <div className="email_div">
-          <input type="email" placeholder="Email" className="email" />
+          <input type="email" placeholder="Email" className="email" name='email' required/>
           <div className="emaildiv">
           </div>
         </div>
         <div className="message_div">
-          <input type="text" placeholder="Message" className="message" />
+          <input type="text" placeholder="Message" className="message" name='message' />
           <div className="messagediv">
           </div>
         </div>
